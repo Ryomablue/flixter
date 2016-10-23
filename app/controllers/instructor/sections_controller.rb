03938edf4cd9e734d2f29741dad1e1b2b94bfa43
Ryomablue <flclfan12@gmail.com>
2016-10-23 22:24:19 +0000

    fixed drag and drop for sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -25,9 +25,19 @@ class Instructor::SectionsController < ApplicationController
     end
   end
 
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render text: "Unauthorized", status: :unauthorized
+    end
+  end
+
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
   end
 
   def current_section
